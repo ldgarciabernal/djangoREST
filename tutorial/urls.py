e73cec579f6e2b0edc9ee1976b1a8af7112adc94
luisdavid <ld.garcia-bernal@4a-side.com>
2019-07-12 12:00:21 +0200

    Step 7. Schemas and client libraries

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -1,6 +1,10 @@
 from django.urls import include, path
 from rest_framework import routers
 from quickstart import views
+from rest_framework.schemas import get_schema_view
+
+
+schema_view = get_schema_view(title='Pastebin API')
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
@@ -9,6 +13,7 @@ router.register(r'groups', views.GroupViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
+    path('schema/', schema_view),
     path('', include(router.urls)),
     path('', include('snippets.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
